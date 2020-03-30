01df8b7d600ebc01d1f2f4e202091333bc1ddfce
August Cayzer <august.cayzer@gousto.co.uk>
2020-03-30 16:10:33 +0100

    adding table

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import TableComponent from "./tableComponent";
+import Header from "./Header";
 
 function App() {
 	return (
 		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<h1>HELLO WORLD!!!!</h1>
-			</header>
+			<Header />
+			<TableComponent />
 		</div>
 	);
 }
---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./Header.css";
+
+class Header extends Component {
+	render() {
+		return (
+			<div>
+				<h3 id="headerLogo">gousto</h3>
+				<h3 id="headerText">GOUSTO TABLE </h3>
+			</div>
+		);
+	}
+}
+
+export default Header;
